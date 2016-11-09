0932ab513661e79fdc1af6cd33ebb44eacb39451
Ashish Misra <amisra@salesforce.com>
2016-11-09 06:39:50 -0800

    First cut of the game algorithm. There might be one test case that fails.

--- src/Facebook/05-game.cpp
+++ src/Facebook/05-game.cpp
@@ -66,11 +66,82 @@ void get_input(vector<int> &test_cases) {
 }
 
 // =======================================================================================
+int num_bits_set(int val) {
+
+	int count = 0;
+
+	while(val) {
+		val = val & (val-1);
+		count++;
+	}
+
+	return count;
+}
+
+int num_moves(const int start_val) {
+
+	if (start_val == 0) {
+		throw runtime_error("start val must be > 0");
+	}
+
+	int moves = 0;
+	int curr_val = start_val;
+	int curr_num_bits = num_bits_set(start_val);
+
+	// First determine power k;
+	int two_power = 1;
+	vector<int> valid_k; 
+
+	while (two_power < start_val) {
+		valid_k.push_back(two_power);
+		two_power = two_power << 1;
+	}
+
+	// Reducing power by 1 to make it less than start_val;
+	two_power = two_power >> 1;
+
+	// TODO: Is there a different loop break condition??
+	for(int i= valid_k.size() - 1; i >= 0; i--) {
+
+		int two_power = valid_k[i];
+		// n - 2^k
+		int tmp_val = curr_val - two_power;
+		int tmp_num_bits = num_bits_set(tmp_val);
+
+		if (tmp_num_bits == curr_num_bits ) {
+			// tmp_val has same beauty as before.
+			curr_val = tmp_val;
+			moves++;
+		}
+	}
+
+	return moves;
+}
+
+void solve(const int start_val) {
+
+	int result = num_moves(start_val);
+
+	if (result % 2) {
+		cout << "First Player" << endl;
+
+	} else {
+		cout << "Second Player" << endl;
+	}
+}
+// =======================================================================================
 
 int main(int argc, const char **argv) {
 
 	vector<int> test_cases;
 
+	// Get all inputs;
 	get_input(test_cases);
 
+	for(int i = 0; i < test_cases.size(); i++) {
+		solve(test_cases[i])	;
+	}
 }
+
+// =======================================================================================
+
